c35555ffb89c5c88c722e8cfd8357f74a072c665
Hiwa-Tashiro <htashiro@sisco-consulting.co.jp>
2024-11-08 14:36:51 +0900

    'nodejs'

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "interntest1108": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "internship14262b09e111": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
